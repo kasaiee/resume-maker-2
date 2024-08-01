b7764fbd3d060113f3e6226bb3a767a0c94fd5cc
mmreza <1tapciran@gmail.com>
2024-08-01 11:01:31 +0330

    editing static root

--- resume_maker/settings.py
+++ resume_maker/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 MEDIA_URL = 'media/'
 
 # Default primary key field type
